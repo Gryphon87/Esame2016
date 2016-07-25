21b56c082583a92b2d7d784e437a9f8916f3754c
gryphon87 <Filippo.Bozzola@gmail.com>
2016-07-25 11:38:52 +0200

    Esame 2016, map reduce

--- mapper.py
+++ mapper.py
@@ -3,7 +3,7 @@ import sys
 def map():
 	for line in sys.stdin:
 		data = line.strip('\n').split(';')
-		year = data[0][4:]
+		year = data[0][:4]
 		if (year == "2016"):
 			paese = data[2]
 			importo = data[6]
